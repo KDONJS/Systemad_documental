8a2ab8f2acecaab77d565d6790056f024bdbe0d5
KDONJS <u19213103@utp.edu.pe>
2022-07-25 14:22:45 -0500

    kdon version 0.0.0.6 categorias

--- template/pages/categorias.php
+++ template/pages/categorias.php
@@ -1,3 +1,160 @@
-<?php   
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <div class="d-flex flex-column-fluid">
+        <div class="container">
 
-    echo "desde la empresa";
+            <div class="card card-custom gutter-b">
+                <div class="card-body">
+                    <!--begin::Top-->
+                    <div class="d-flex">
+                        <!--begin::Pic-->
+                        <div class="flex-shrink-0 mr-7">
+                            <div class="symbol symbol-50 symbol-lg-120">
+                                <img alt="Pic" src="https://images.pexels.com/photos/247676/pexels-photo-247676.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                            </div>
+                        </div>
+                        <!--end::Pic-->
+                        <!--begin: Info-->
+                        <div class="flex-grow-1">
+                            <!--begin::Title-->
+                            <div class="d-flex align-items-center justify-content-between flex-wrap mt-2">
+                                <!--begin::User-->
+                                <div class="mr-3">
+                                    <!--begin::Name-->
+                                    <a href="#"
+                                        class="d-flex align-items-center text-dark text-hover-primary font-size-h5 font-weight-bold mr-3">Ciancia</a>
+                                    <!--end::Name-->
+                                   
+                                </div>
+                                <!--begin::User-->
+                                <!--begin::Actions-->
+                                <div class="my-lg-0 my-1">
+                                    <a href="mailto:ejemplo@gmail.com" class="btn btn-sm btn-light-primary font-weight-bolder mr-2">Enviame una sugerencia</a>
+                                </div>
+                                <!--end::Actions-->
+                            </div>
+                            <!--end::Title-->
+                            <!--begin::Content-->
+                            <div class="d-flex align-items-center flex-wrap justify-content-between">
+                                <!--begin::Description-->
+                                <div class="flex-grow-1 font-weight-bold text-dark-50 py-2 py-lg-2 mr-5">
+                                    En esta categoria podras encontrar todo lo que necesitas para el desarrollo de las ciencia.
+                                </div>
+                                <!--end::Description-->
+                                <!--begin::Progress-->
+                                <div class="d-flex mt-4 mt-sm-0">
+                                    <span class="font-weight-bold mr-4">Progreso</span>
+                                    <div class="progress progress-xs mt-2 mb-2 flex-shrink-0 w-150px w-xl-250px">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: 50%;"
+                                            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <span class="font-weight-bolder text-dark ml-4">78%</span>
+                                </div>
+                                <!--end::Progress-->
+                            </div>
+                            <!--end::Content-->
+                        </div>
+                        <!--end::Info-->
+                    </div>
+                    <!--end::Top-->
+                    <!--begin::Separator-->
+                    <div class="separator separator-solid my-7"></div>
+                    <!--end::Separator-->
+                    <!--begin::Bottom-->
+                    <div class="d-flex align-items-center flex-wrap">
+                       
+                        
+                    
+                        <!--begin: Item-->
+                        <div class="d-flex align-items-center flex-lg-fill mr-5 my-1">
+                            <span class="mr-4">
+                                <span class="svg-icon svg-icon-2x">
+                                    <!--begin::Svg Icon | path:/keen/theme/demo3/dist/assets/media/svg/icons/Shopping/Chart-bar1.svg-->
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"></rect>
+                                            <rect fill="#000000" opacity="0.3" x="12" y="4" width="3" height="13"
+                                                rx="1.5"></rect>
+                                            <rect fill="#000000" opacity="0.3" x="7" y="9" width="3" height="8"
+                                                rx="1.5"></rect>
+                                            <path
+                                                d="M5,19 L20,19 C20.5522847,19 21,19.4477153 21,20 C21,20.5522847 20.5522847,21 20,21 L4,21 C3.44771525,21 3,20.5522847 3,20 L3,4 C3,3.44771525 3.44771525,3 4,3 C4.55228475,3 5,3.44771525 5,4 L5,19 Z"
+                                                fill="#000000" fill-rule="nonzero"></path>
+                                            <rect fill="#000000" opacity="0.3" x="17" y="11" width="3" height="6"
+                                                rx="1.5"></rect>
+                                        </g>
+                                    </svg>
+                                    <!--end::Svg Icon-->
+                                </span>
+                            </span>
+                            <div class="d-flex flex-column text-dark-75">
+                                <span class="font-weight-bolder font-size-sm">Net</span>
+                                <span class="font-weight-bolder font-size-h5">
+                                    <span class="text-dark-50 font-weight-bold">$</span>782,300</span>
+                            </div>
+                        </div>
+                        <!--end: Item-->
+                        <!--begin: Item-->
+                        <div class="d-flex align-items-center flex-lg-fill mr-5 my-1">
+                            <span class="mr-4">
+                                <span class="svg-icon svg-icon-2x">
+                                    <!--begin::Svg Icon | path:/keen/theme/demo3/dist/assets/media/svg/icons/Tools/Hummer.svg-->
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"></rect>
+                                            <path
+                                                d="M18.4246212,12.6464466 L21.2530483,9.81801948 C21.4483105,9.62275734 21.764893,9.62275734 21.9601551,9.81801948 L22.6672619,10.5251263 C22.862524,10.7203884 22.862524,11.0369709 22.6672619,11.232233 L19.8388348,14.0606602 C19.6435726,14.2559223 19.3269901,14.2559223 19.131728,14.0606602 L18.4246212,13.3535534 C18.2293591,13.1582912 18.2293591,12.8417088 18.4246212,12.6464466 Z M3.22182541,17.9497475 L13.1213203,8.05025253 C13.5118446,7.65972824 14.1450096,7.65972824 14.5355339,8.05025253 L15.9497475,9.46446609 C16.3402718,9.85499039 16.3402718,10.4881554 15.9497475,10.8786797 L6.05025253,20.7781746 C5.65972824,21.1686989 5.02656326,21.1686989 4.63603897,20.7781746 L3.22182541,19.363961 C2.83130112,18.9734367 2.83130112,18.3402718 3.22182541,17.9497475 Z"
+                                                fill="#000000" opacity="0.3"></path>
+                                            <path
+                                                d="M12.3890873,1.28248558 L12.3890873,1.28248558 C15.150511,1.28248558 17.3890873,3.52106183 17.3890873,6.28248558 L17.3890873,10.7824856 C17.3890873,11.058628 17.1652297,11.2824856 16.8890873,11.2824856 L12.8890873,11.2824856 C12.6129449,11.2824856 12.3890873,11.058628 12.3890873,10.7824856 L12.3890873,1.28248558 Z"
+                                                fill="#000000"
+                                                transform="translate(14.889087, 6.282486) rotate(-45.000000) translate(-14.889087, -6.282486)">
+                                            </path>
+                                        </g>
+                                    </svg>
+                                    <!--end::Svg Icon-->
+                                </span>
+                            </span>
+                            <div class="d-flex flex-column flex-lg-fill">
+                                <span class="text-dark-75 font-weight-bolder font-size-sm">73 Projects</span>
+                                <a href="#" class="text-primary font-weight-bolder">View</a>
+                            </div>
+                        </div>
+                        <!--end: Item-->
+                        <!--begin: Item-->
+                        <div class="d-flex align-items-center flex-lg-fill mr-5 my-1">
+                            <span class="mr-4">
+                                <span class="svg-icon svg-icon-2x">
+                                    <!--begin::Svg Icon | path:/keen/theme/demo3/dist/assets/media/svg/icons/Communication/Chat2.svg-->
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"></rect>
+                                            <polygon fill="#000000" opacity="0.3" points="5 15 3 21.5 9.5 19.5">
+                                            </polygon>
+                                            <path
+                                                d="M13.5,21 C8.25329488,21 4,16.7467051 4,11.5 C4,6.25329488 8.25329488,2 13.5,2 C18.7467051,2 23,6.25329488 23,11.5 C23,16.7467051 18.7467051,21 13.5,21 Z M9,8 C8.44771525,8 8,8.44771525 8,9 C8,9.55228475 8.44771525,10 9,10 L18,10 C18.5522847,10 19,9.55228475 19,9 C19,8.44771525 18.5522847,8 18,8 L9,8 Z M9,12 C8.44771525,12 8,12.4477153 8,13 C8,13.5522847 8.44771525,14 9,14 L14,14 C14.5522847,14 15,13.5522847 15,13 C15,12.4477153 14.5522847,12 14,12 L9,12 Z"
+                                                fill="#000000"></path>
+                                        </g>
+                                    </svg>
+                                    <!--end::Svg Icon-->
+                                </span>
+                            </span>
+                            <div class="d-flex flex-column">
+                                <span class="text-dark-75 font-weight-bolder font-size-sm">648 Comments</span>
+                                <a href="#" class="text-primary font-weight-bolder">View</a>
+                            </div>
+                        </div>
+                        <!--end: Item-->
+                        
+                    </div>
+                    <!--end::Bottom-->
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
